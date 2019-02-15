beefebcf87ac0a26796f3d526c21202749c5e7d6
griegomas <38501062+griegomas@users.noreply.github.com>
2019-02-14 19:20:43 -0500

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Clicky Game!</h1>
+    <h2>
+      Click on ANY image to earn - clicking on the same image twice is a no-no.
+    </h2>
+  </header>
+);
+
+export default Header;
---
+++ src/components/NavMessage/NavMessage.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./NavMessage.css";
+
+class NavMessage extends Component {
+  state = {
+    message: "",
+    animating: false
+  };
+
+  componentWillReceiveProps({ score, topScore }) {
+    let newState = { animating: true };
+    if (score === 0 && topScore === 0) {
+      newState.message = "";
+    } else if (score === 0 && topScore > 0) {
+      newState.message = "incorrect";
+    } else {
+      newState.message = "correct";
+    }
+    this.setState(newState, () =>
+      setTimeout(() => this.setState({ animating: false }), 500)
+    );
+  }
+
+  renderMessage = () => {
+    switch (this.state.message) {
+      case "correct":
+        return "Correct!";
+      case "incorrect":
+        return "Failure :-(";
+      default:
+        return "Click an image to begin!";
+    }
+  };
+
+  render() {
+    return (
+      <li className={this.state.animating ? this.state.message : ""}>
+        {this.renderMessage()}
+      </li>
+    );
+  }
+}
+
+export default NavMessage;
